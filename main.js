87a9d6693ba6fe5f5373cc29e2cc80d028b70aef
Valentyn13 <valentyn13git@gmail.com>
2022-04-26 20:30:05 +0300

    start to describe add categories event

--- main.js
+++ main.js
@@ -2,4 +2,20 @@
 
 const newCategoryForm = document.querySelector('[data-new-categore-form]');
 const newCategoryInput = document.querySelector('[data-new-categore-input');
-const categoriesContainer = document.querySelector('[data-categories]')
+const categoriesContainer = document.querySelector('[data-categories]');
+
+const LOCAL_STORAGE_CATEGORIES_KEYS = 'LOCAL_STORAGE_CATEGORIES_KEYS';
+
+// Add categories
+newCategoryForm.addEventListener('submit', (event) => {
+    event.preventDefault; // Форма не будет обновлятся/сбрасиватся
+
+    // Get value from input field
+    const category = newCategoryInput.value;
+
+    // Validation
+    const isCategoryEmpty = !category || !category.trim().length;
+    if (isCategoryEmpty) {
+        return console.log('Enter the task!');
+    }
+})
