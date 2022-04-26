e11caed88be0f36e4dc72ec365cc1197003d31b8
Valentyn13 <valentyn13git@gmail.com>
2022-04-26 19:58:14 +0300

    link javascript file

---
+++ main.js
@@ -0,0 +1,5 @@
+'use strict'
+
+const newCategoryForm = document.querySelector('[data-new-categore-form]');
+const newCategoryInput = document.querySelector('[data-new-categore-input');
+const categoriesContainer = document.querySelector('[data-categories]')
